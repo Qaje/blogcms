91f3de9011c4dbd995c10584493c9b3f1c990e39
qaje <javierpal3232@gmail.com>
2018-03-05 10:03:25 -0400

    add validator from with parsley

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+//use App\Http\Request;
+//use App\Http\Controllers\Controller;
+use App\Post;
+
 class PostController extends Controller
 {
     /**
@@ -34,8 +38,21 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        // validar los datos
+        $this->validate($request, array(
+            'title' => 'required|max:255',
+            'body' => 'required'
+        ));
+        // almacenar en la base de datos
+        $post = new Post;
+
+        $post->title = $request->title;
+        $post->body  = $request->body;
+
+        $post->save();
+        // redireccionar aotra pagina
+        return redirect()->route('posts.show', $post->id);
+    }   
 
     /**
      * Display the specified resource.
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,6 +2,12 @@
 
 @section('title', '| Create New Post')
 
+@section('stylesheet')
+
+	{!! Html::style('css/parsley.css') !!}
+
+@endsection 
+
 @section('content')
 
 	<div class="row justify-content-center">
@@ -9,12 +15,12 @@
 			<h1>Create New Post</h1>
 			<hr>
 
-			{!! Form::open(['route' => 'posts.store']) !!}
+			{!! Form::open(['route' => 'posts.store', 'data-parsley-validate' => 'true'	]) !!}
 			    {{ Form::label('title','Title:') }}
-			    {{ Form::text('title',null, array('class'=>'form-control')) }}
+			    {{ Form::text('title',null, array('class'=>'form-control', 'required' => '' , 'maxlength'=>'6')) }}
 
 			    {{ Form::label('body','Post Body:') }}
-			    {{ Form::textarea('body',null, array('class'=>'form-control')) }}
+			    {{ Form::textarea('body',null, array('class'=>'form-control', 'required' => '')) }}
 
 			    {{Form::submit('Create Post',array('class'=> 'btn btn-success btn-lg btn-block', 'style' => 'margin-top: 20px'))}}
 			{!! Form::close() !!}
@@ -22,3 +28,9 @@
 	</div>
 
 @endsection
+
+@section('scripts')
+
+	{!! Html::script('js/parsley.min.js') !!}
+
+@endsection 
