14ef6aa405bbdff2d6bf8a2cfd25589f10f8dc20
JAPO <javierpal3232@gmail.com>
2018-04-17 14:40:23 -0400

    contact me in my email

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,7 +7,11 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+//use App\Http\Request;
 use App\Post;
+use Illuminate\Http\Request;
+use Mail;
+use Session;
 
 class PagesController extends BaseController{
 
@@ -30,4 +34,31 @@ class PagesController extends BaseController{
 		return view('pages.contact');
 	}
 
+
+	public function postContact(Request $request){
+		/*
+		$this->validated($request,[
+			'email' => 'required|email',
+			'subject'=> 'min:3',
+			'message'=> 'min:10']);
+		*/	
+		$data =  array(
+			'email' => $request->email,
+			'subject' => $request->subject,
+			'bodyMessage' => $request->message
+		);
+
+
+		Mail::send('emails.contact',$data,function($message) use ($data){
+			$message->from($data['email']);
+			$message->to('hello@devmarka.io');
+			$message->subject($data['subject']);
+
+		});
+
+		Session::flash('success','Your email was sent !!');
+
+		return redirect()->route('pages.welcome');
+ 
+	}
 }
